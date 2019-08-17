d9323f72941efaa87a9695ed9ba6dbaa41067c94
Rohan Narain <rohannarain@Rohans-MacBook-Pro-3.local>
2019-08-16 20:02:38 -0700

    website update

--- web_assets/script/main.js
+++ web_assets/script/main.js
@@ -6,9 +6,20 @@ var months = ['January','February','March','April','May','June','July',
 'August','September','October','November','December']; 
 var today = new Date();
 today.setTime(today.getTime());
-document.getElementById("span-date").innerHTML = months[today.getMonth()] + " " + today.getDate()+ ", " + today.getFullYear();
+document.getElementById("span-date").innerHTML = months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
 
-d3.text("https://raw.githubusercontent.com/rohannarain/beat_the_streak/master/data/predictions/predictions_08_15_2019.csv", function(data) {
+var monthNumbers = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+var fileMonth = monthNumbers[today.getMonth()]
+
+var fileDate = today.getDate();
+if (fileDate < 10) {
+    fileDate = "0" + String(fileDate)
+}
+
+var fileYear = today.getFullYear();
+
+d3.text("https://raw.githubusercontent.com/rohannarain/beat_the_streak/master/data/predictions/predictions_${fileMonth}_${fileDate}_${fileYear}.csv", 
+    function(data) {
     var parsedCSV = d3.csv.parseRows(data);
 
     var container = d3.select("header")
