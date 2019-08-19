da82b63a3568e07b4f841b576bac23d593b7da79
Rohan Narain <rohannarain@rohans-mbp-3.attlocal.net>
2019-08-19 15:41:16 -0700

    Creating a 'past results' page for the website to display accuracy over past 7 days

---
+++ web_assets/script/past_results.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+  $("body").height($(window).height());
+})
+
+var months = ['January','February','March','April','May','June','July',
+'August','September','October','November','December']; 
+var today = new Date();
+today.setTime(today.getTime());
+document.getElementById("span-date").innerHTML = months[today.getMonth()] + " " + today.getDate() + ", " + today.getFullYear();
+
+var monthNumbers = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+var fileMonth = monthNumbers[today.getMonth()]
+
+var fileDate = today.getDate();
+if (fileDate < 10) {
+    fileDate = "0" + String(fileDate)
+}
+
+var fileYear = today.getFullYear();
+var getURL = "https://raw.githubusercontent.com/rohannarain/beat_the_streak/master/data/predictions/predictions_" + fileMonth + "_" + fileDate + "_" + fileYear + ".csv" 
+
+// d3.text(getURL, function(data) {
+//     var parsedCSV = d3.csv.parseRows(data);
+
+//     var container = d3.select("header")
+//         .append("table")
+
+//     .selectAll("tr")
+//         .data(parsedCSV).enter()
+//         .append("tr")
+
+//     .selectAll("td")
+//         .data(function(d) {
+//             return d;
+//         }).enter()
+//         .append("td")
+//         .text(function(d) {
+//             return d;
+//         });
+// });
